81b3c9f161cf0bc2182de897e08a65022d4c9d20
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-02 07:29:57 -0600

    Add cors

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var path = require('path');
 var favicon = require('serve-favicon');
 
 var app = express();
+var cors = require('cors');
 
+app.use(cors);
 require('./bootstrap/middleware')(app);
 require('./bootstrap/session')(app);
 require('./bootstrap/mongo');
